9f686c8114b86bbf01c701d26fa692f23374042e
Tuyen94 <tuyenbkk57@gmail.com>
2022-08-15 22:01:43 +0700

    Init dcn-core

---
+++ dcn-core/src/main/java/tuyen/bui/sms/DcnCoreApplication.java
@@ -0,0 +1,13 @@
+package tuyen.bui.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcnCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcnCoreApplication.class, args);
+	}
+
+}
---
+++ dcn-core/src/test/java/tuyen/bui/sms/DcnCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tuyen.bui.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcnCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
